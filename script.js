eb20a9a41f86c8dc192d6179109e25317f7d35c6
dhristi <dhristisingh831@gmail.com>
2025-03-10 22:19:16 +0530

    added responsive menu

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const menuElement = document.querySelector(".menu");
 const menuOptionsElement = document.querySelector('.menu-options');
-console.log(menuElement);
+const menuIconElement = document.querySelector('.menu-icon');
+const crossIconElement = document.querySelector('.cross-icon');
 
+let isShowing = false;
 menuElement.addEventListener("click", () => {
-    console.log('hello');
     menuOptionsElement.classList.toggle('flex');
+    if( !isShowing ){
+        menuIconElement.style.display = "none";
+        crossIconElement.style.display = "flex";
+        crossIconElement.style.opacity = "1";
+        isShowing = true
+    }else {
+        menuIconElement.style.display = "flex";
+        crossIconElement.style.display = "none";
+        crossIconElement.style.opacity = "0";
+        isShowing = false;
+    }
 });
